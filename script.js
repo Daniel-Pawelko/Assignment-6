2d626254d4e0db213404c93986ac95b573cc34a1
DanielPawelko <daniel.pawelko@ocsbstudent.ca>
2021-01-13 14:37:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('button-id').addEventListener('click', calc)
+const output = document.getElementById('output-id')
+
+function calc(){
+  size = document.getElementById('input-size').value
+  a = document.getElementById('input-toppings').value
+  if(a=='1'){a=1} 
+  if(a=='2'){a=1.75} 
+  if(a=='3'){a=2.50} 
+  if(a=='4'){a=3.35} 
+  console.log(a)
+  if (size == 'xlarge'){
+    output.innerHTML = (10+a)*1.13
+  }
+  else if (size == 'large'){
+    output.innerHTML = (6+a)*1.13
+  }
+}
